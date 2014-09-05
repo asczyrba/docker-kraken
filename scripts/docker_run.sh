504a868418374051997e974b725135ee2d928af6
Ubuntu <ubuntu@ip-10-75-34-232.eu-west-1.compute.internal>
2014-09-05 10:23:11 +0000

    less parameters

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
 # script to submit to qsub array job, that calls "docker run..."
-# -> qsub -t 1-<num_tasks> -cwd docker_run.sh <num_tasks> <filename> ...	
+# -> qsub -t 1-<num_tasks> -cwd docker_run.sh LOCALDIR SPOOLDIR COMMAND
 
-if [ $# -ne 6 ]
+if [ $# -ne 3 ]
   then
-    echo "Usage: docker_run.sh NUM_TASKS INFILE INDIR DBDIR OUTDIR COMMAND"
+    echo "Usage: docker_run.sh LOCALDIR SPOOLDIR COMMAND"
     exit 0;
 fi
 
-NUM_TASKS=$1
-INFILE=$2
-INDIR=$3
-DBDIR=$4
-OUTDIR=$5
-COMMAND=$6
+LOCALDIR=$1
+SPOOLDIR=$2
+COMMAND=$3
 
 sudo docker run \
-    -e "NUM_TASKS=$NUM_TASKS" \
-    -e "TASK_ID=$SGE_TASK_ID" \
-    -e "INFILE=$INFILE" \
-    -v $INDIR:/vol/in \
-    -v $DBDIR:/vol/db \
-    -v $OUTDIR:/vol/out \
+    -e "SGE_TASK_LAST=$SGE_TASK_LAST" \
+    -e "SGE_TASK_ID=$SGE_TASK_ID" \
+    -v $LOCALDIR:/vol/scratch \
+    -v $SPOOLDIR:/vol/spool \
     asczyrba/kraken \
     $COMMAND
 

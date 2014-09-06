c05698a4673ae5e4ec9b980b07bb9a5ed23a97aa
Ubuntu <ubuntu@ip-10-75-34-232.eu-west-1.compute.internal>
2014-09-06 10:06:09 +0000

    new

--- scripts/kraken_pipeline.pl
+++ scripts/kraken_pipeline.pl
@@ -1,32 +1,36 @@
 #!/usr/bin/env perl
 
 use Getopt::Long;
+use File::Path;
 use Sys::Hostname;
 use strict;
 
-my ($s3src, $infile, $krakendb, $grid_nodes, $current_node);
+my ($infile, $krakendb, $grid_nodes, $current_node);
 
 
-GetOptions("s3src=s"        => \$s3src,
-	   "infile=s"       => \$infile,
+GetOptions("infile=s"       => \$infile,
 	   "krakendb=s"     => \$krakendb
     );
 
 $grid_nodes = $ENV{"SGE_TASK_LAST"};
 $current_node = $ENV{"SGE_TASK_ID"};
 
-unless ($s3src && $infile && $krakendb) {
-    die "\nusage: $0 -s3src S3URL -infile FASTQFILE -krakendb KRAKEN_DB
+unless ($infile && $krakendb) {
+    die "\nusage: $0 -infile FASTQFILE_S3_URL -krakendb KRAKEN_DB_S3_URL
+
+Example: kraken_pipeline.pl -krakendb s3://bibicloud-demo/kraken-db/minikraken_20140330 -infile s3://bibicloud-demo/HMP_Stool_Sample/SRS011405_20M.fastq
 
 ";
 }
 
 my $host = hostname;
+my $krakendb_dir = '/vol/scratch/krakendb';
+mkpath($krakendb_dir);
 print STDERR "host: $host, SGE_LAST_TASK: $grid_nodes, SGE_TASK_ID: $current_node\n";
 
 print STDERR "Donwloading Database to /vol/scratch/krakendb/...\n";
-print STDERR "/vol/scripts/download.pl -type folder -source $krakendb/ -dest /vol/scratch/krakendb/\n";
-system("/vol/scripts/download.pl -type folder -source $krakendb/ -dest /vol/scratch/krakendb/");
+print STDERR "/vol/scripts/download.pl -type folder -source $krakendb/ -dest $krakendb_dir\n";
+system("/vol/scripts/download.pl -type folder -source $krakendb/ -dest $krakendb_dir");
 print STDERR "Done downloading Database.\n";
 
 print STDERR "Donwloading FASTQ File to /vol/scratch/...\n";

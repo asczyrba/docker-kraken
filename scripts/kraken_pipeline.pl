5463bcfee962d67070ac0a7f53b95fbd390f64fc
Ubuntu <ubuntu@ip-10-75-34-232.eu-west-1.compute.internal>
2014-09-06 08:18:54 +0000

    new

--- scripts/kraken_pipeline.pl
+++ scripts/kraken_pipeline.pl
@@ -22,4 +22,16 @@ unless ($s3src && $infile && $krakendb) {
 }
 
 my $host = hostname;
-print "host: $host, SGE_LAST_TASK: $grid_nodes, SGE_TASK_ID: $current_node\n";
+print STDERR "host: $host, SGE_LAST_TASK: $grid_nodes, SGE_TASK_ID: $current_node\n";
+
+print STDERR "Donwloading Database to /vol/scratch/krakendb/...\n";
+print STDERR "/vol/scripts/download.pl -type folder -source $krakendb/ -dest /vol/scratch/krakendb/\n";
+system("/vol/scripts/download.pl -type folder -source $krakendb/ -dest /vol/scratch/krakendb/");
+print STDERR "Done downloading Database.\n";
+
+print STDERR "Donwloading FASTQ File to /vol/scratch/...\n";
+print STDERR "/vol/scripts/download.pl -type split-fastq -source $infile -dest /vol/scratch  -grid-nodes $grid_nodes -current-node $current_node\n";
+system("/vol/scripts/download.pl -type split-fastq -source $infile -dest /vol/scratch  -grid-nodes $grid_nodes -current-node $current_node");
+print STDERR "Done downloading FASTQ file.\n";
+
+
